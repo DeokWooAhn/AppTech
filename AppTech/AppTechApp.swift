bba7c83efbc5dd1bef18ea5364b9d794aa1ac4ad
dwahn <dwahn@anytale.com>
2025-09-28 13:27:58 +0900

    Initial Commit

---
+++ AppTech/AppTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTechApp.swift
+//  AppTech
+//
+//  Created by AhnDeokWoo on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AppTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTechTests/AppTechTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTechTests.swift
+//  AppTechTests
+//
+//  Created by AhnDeokWoo on 9/28/25.
+//
+
+import Testing
+@testable import AppTech
+
+struct AppTechTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
